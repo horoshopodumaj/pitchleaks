5db3c802b6a4c18268798eeb24a4e8b19352218b
Diana Murtazina <murka8831@yandex.ru>
2023-03-21 16:51:13 +0400

    deleting unnecessary files

--- pages/_layout/_headers/CommonHeaderRight.tsx
+++ pages/_layout/_headers/CommonHeaderRight.tsx
@@ -1,34 +1,14 @@
 import React, { FC, ReactNode, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useTranslation } from 'next-i18next';
-import classNames from 'classnames';
-import { useTour } from '@reactour/tour';
 import Button, { IButtonProps } from '../../../components/bootstrap/Button';
 import { HeaderRight } from '../../../layout/Header/Header';
-import OffCanvas, {
-	OffCanvasBody,
-	OffCanvasHeader,
-	OffCanvasTitle,
-} from '../../../components/bootstrap/OffCanvas';
-import Alert from '../../../components/bootstrap/Alert';
-import Dropdown, {
-	DropdownItem,
-	DropdownMenu,
-	DropdownToggle,
-} from '../../../components/bootstrap/Dropdown';
-import Icon from '../../../components/icon/Icon';
 import ThemeContext from '../../../context/themeContext';
-import LANG, { getLangWithKey, ILang } from '../../../lang';
-import showNotification from '../../../components/extras/showNotification';
 import useDarkMode from '../../../hooks/useDarkMode';
-import Popovers from '../../../components/bootstrap/Popovers';
-import Spinner from '../../../components/bootstrap/Spinner';
 import useMounted from '../../../hooks/useMounted';
 import { useRouter } from 'next/router';
 import Logo from '../../../assets/images/logo.svg';
-import { WrapperContainer } from '../../../layout/Wrapper/Wrapper';
 import Image from 'next/image';
-import { useMeasure, useWindowSize } from 'react-use';
+import { useWindowSize } from 'react-use';
 import Link from 'next/link';
 import ScrollspyNav from '../../../components/bootstrap/ScrollspyNav';
 
@@ -37,7 +17,6 @@ interface ICommonHeaderRightProps {
 	afterChildren?: ReactNode;
 }
 const CommonHeaderRight: FC<ICommonHeaderRightProps> = ({ beforeChildren, afterChildren }) => {
-	const router = useRouter();
 	const { darkModeStatus, setDarkModeStatus } = useDarkMode();
 
 	const { fullScreenStatus, setFullScreenStatus, rightMenuStatus, setRightMenuStatus } =
@@ -49,25 +28,7 @@ const CommonHeaderRight: FC<ICommonHeaderRightProps> = ({ beforeChildren, afterC
 		size: 'lg',
 	};
 
-	const [offcanvasStatus, setOffcanvasStatus] = useState(false);
-
-	const { i18n } = useTranslation();
-
-	const changeLanguage = (lng: ILang['key']['lng']) => {
-		i18n.changeLanguage(lng);
-		router.push(router.pathname, router.pathname, { locale: lng });
-		showNotification(
-			<span className='d-flex align-items-center'>
-				<Icon icon={getLangWithKey(lng)?.icon} size='lg' className='me-1' />
-				<span>{`Language changed to ${getLangWithKey(lng)?.text}`}</span>
-			</span>,
-			'You updated the language of the site. (Only "Aside" was prepared as an example.)',
-		);
-	};
-
 	const { mounted } = useMounted();
-
-	const { setIsOpen } = useTour();
 
 	const windowsWidth = useWindowSize().width;
 
